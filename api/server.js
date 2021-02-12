c3657ebae3b3cccbbb04463edbcd4279bb59463f
Danila Koltcov <uolcovd@gmail.com>
2021-02-12 12:31:34 +0300

    Add controller collector

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const config = require('./config').database;
 const newCard = require('./routes/cards/new-card.post');
 const allCards = require('./routes/cards/cards.get');
 const renameCard = require('./routes/cards/rename-card.put')
@@ -12,23 +12,40 @@ const toggleDone = require('./routes/cards/toggle-done-card.put')
 
 const app = express();
 
-const sequelize = new Sequelize(config.dbName, config.user, config.password, {
-  dialect: config.dialect,
-  host: config.host,
+const sequelize = new Sequelize(process.env.dbName, process.env.user, process.env.password, {
+  dialect: process.env.dialect,
+  host: process.env.host,
 });
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const jsonParser = bodyParser.json({ extended: false });
 
+
+
 app.use(cors());
 app.use(urlencodedParser);
 app.use(jsonParser);
 
-app.use('/newcard', newCard);
-app.use('/allcards', allCards);
-app.use('/renamecard', renameCard);
-app.use('/deletecard', deleteCard)
-app.use('/toggledone', toggleDone)
+const klawSync = require('klaw-sync');
+const path = require('path')
+async function useControllers() {
+    const paths = klawSync(`${__dirname}/routes/cards`, {nodir: true});
+    let controllersCount = 0;
+    paths.forEach((file) => {
+        if (path.basename(file.path)[0] === '_' || path.basename(file.path)[0] === '.') return;
+        app.use('/', require(file.path));
+        controllersCount++;
+    });
+
+    console.info(`Total controllers: ${controllersCount}`);
+};
+useControllers()
+// app.use('/newcard', newCard);
+// app.use('/allcards', allCards);
+// app.use('/renamecard', renameCard);
+// app.use('/deletecard', deleteCard)
+// app.use('/toggledone', toggleDone)
+
 
 sequelize
   .sync()
