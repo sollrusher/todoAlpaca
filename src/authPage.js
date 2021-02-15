7df9aec420eee4deb70e70be4002d38751c9d4b3
Danila Koltcov <uolcovd@gmail.com>
2021-02-15 15:50:01 +0300

    Add methods to login and registration

--- src/authPage.js
+++ src/authPage.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
-import './authPage.css'
+import React, { Component } from 'react';
+import './authPage.css';
+import api from './utils/api';
 
 export default class Auth extends Component {
   constructor(props) {
     super(props);
     this.state = {
       login: '',
-      password: ''
+      password: '',
     };
   }
 
@@ -16,31 +17,47 @@ export default class Auth extends Component {
     const name = target.name;
 
     this.setState({
-      [name]: value
+      [name]: value,
     });
-  }
+  };
 
-    // handleEnter = (e) => {
-  // const re= /\S+@\S+\.\S+/;
-  // if(!re.test(value)){
-  //   return
-  // }
-  // }
+  handleSubmit = async (event) => {
+    if (event.key == 'Enter' && this.state.login !== '') {
+
+      const { login, password } = this.state
+      const data = await api.post('/login', {login, password})
+      console.log(data.data.id)
+    }
+  };
 
   render() {
     return (
       <section className="auth">
         <div className="auth__inputs">
           <p>
-            Логин: <input type="text" name='login' value={this.state.login} onChange={this.handleInputChange}/>
+            Логин:
+            <input
+              type="text"
+              name="login"
+              value={this.state.login}
+              onChange={this.handleInputChange}
+              onKeyPress={this.handleSubmit}
+            />
           </p>
           <p>
-            Пароль: <input type="password" name='password' value={this.state.password} onChange={this.handleInputChange}/>
+            Пароль:
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              onKeyPress={this.handleSubmit}
+            />
           </p>
         </div>
         <div className="auth__buttons">
-            {/* <input type="button" value="Регистрация"></input> */}
-            <input type="button" value="Войти"/>
+          {/* <input type="button" value="Регистрация"></input> */}
+          <input type="button" value="Войти" />
         </div>
       </section>
     );
--- src/registerPage.js
+++ src/registerPage.js
@@ -1,21 +1,59 @@
 import React, { Component } from "react";
 import './registerPage.css'
+import api from "./utils/api";
 
 export default class Register extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      login: '',
+      password: '',
+    };
   }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = async (event) => {
+    if (event.key == 'Enter' && this.state.login !== '') {
+
+      const { login, password } = this.state
+      const data = await api.post('/signup', {login, password})
+      console.log(data.data.id)
+    }
+  };
 
   render() {
     return (
       <section className="register">
           <h2>Регистрация</h2>
         <div className="register__inputs">
-          <p>
-            Логин: <input type="text"></input>
+        <p>
+            Логин:
+            <input
+              type="text"
+              name="login"
+              value={this.state.login}
+              onChange={this.handleInputChange}
+              onKeyPress={this.handleSubmit}
+            />
           </p>
           <p>
-            Пароль: <input type="password"></input>
+            Пароль:
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              onKeyPress={this.handleSubmit}
+            />
           </p>
         </div>
         <div className="register__buttons">
