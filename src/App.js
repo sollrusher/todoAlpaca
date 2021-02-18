80ccf8064ac6d94214ec97ee6b833c74ffcffde0
Danila Koltcov <uolcovd@gmail.com>
2021-02-18 11:42:30 +0300

    Add logout button to App

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 
-import Auth from "./authPage";
-import "./app.css";
-import Register from "./registerPage";
-import TodoList from "./todoList";
+import Auth from './authPage';
+import './app.css';
+import Register from './registerPage';
+import TodoList from './todoList';
 import PrivateRoute from './utils/privateRoute';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
+import { logoutUser } from './store/action/action';
 
 const mapStateToProps = (store) => ({
   initialized: store.initialized,
   store,
 });
 
-const connector = connect(mapStateToProps);
+const mapDispatchToProps = {
+  logoutUser,
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
 
 function App(props) {
-  const { initialized } = props;
-
-  console.log(initialized);
-  
+  const { initialized, logoutUser } = props;
+  const classes = `logout ${initialized ? '' : 'off'}`;
   return (
-    <Router >
-      <ul>
-        <li>
-          <Link to="/">Войти</Link>
-        </li>
-        <li>
-          <Link to="/register">Регистрация</Link>
-        </li>
-        <li>
-          <Link to="/todolist">ToDo List</Link>
-        </li>
-      </ul>
+    <Router>
+      <input
+        className={classes}
+        type="button"
+        value="Выйти"
+        onClick={() => logoutUser()}
+      />
+
       <div className="app">
-        <Switch >
+        <Switch>
           <Route exact path="/">
             {initialized ? <Redirect to="/todolist" /> : <Auth />}
           </Route>
           <Route path="/register">
-          {initialized ? <Redirect to="/todolist" /> : <Register/>}
+            {initialized ? <Redirect to="/todolist" /> : <Register />}
           </Route>
           <PrivateRoute path="/todolist" auth={initialized}>
-            <TodoList/>
+            <TodoList />
           </PrivateRoute>
         </Switch>
-        </div>
+      </div>
     </Router>
   );
 }
