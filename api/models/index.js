9c163dc104b979d2ddab20193f69b92ebc3dd5c8
Danila Koltcov <uolcovd@gmail.com>
2021-02-24 12:30:30 +0300

    Update models/index

--- api/models/index.js
+++ api/models/index.js
@@ -14,7 +14,12 @@ console.log('ENVENVENV', env)
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config, {
+    ssl: true,
+    dialectOptions: {
+      ssl: true
+  }
+});
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
