e53523f29698bd5b75c0f6efdf1d143d58fa268e
Danila Koltcov <uolcovd@gmail.com>
2021-02-24 12:11:49 +0300

    Change config

--- api/config/config.js
+++ api/config/config.js
@@ -16,6 +16,6 @@ module.exports = {
       DATABASE_URL: process.env.DATABASE_URL,      
     },
     production: {
-      use_env_variable: DATABASE_URL,        
+      use_env_variable: process.env.DATABASE_URL,        
     }
   }
