222ad870c1d6743e5c40ddda66dfbb06a6786d66
Danila Koltcov <uolcovd@gmail.com>
2021-02-25 18:38:26 +0300

    Change sequelize-config

--- api/sequelize-config.js
+++ api/sequelize-config.js
@@ -2,4 +2,12 @@ require('dotenv').config();
 module.exports = {
         url: process.env.DATABASE_URL,
         dialect: "postgres",      
+        ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 }
