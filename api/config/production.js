e7fe8aec2871a7135d5dbb5cbc9ae8f1589bb241
Danila Koltcov <uolcovd@gmail.com>
2021-02-25 16:20:56 +0300

    Add ssl to production config

--- api/config/production.js
+++ api/config/production.js
@@ -1,13 +1,12 @@
 require('dotenv').config({ path: '../.env' });
 module.exports = {
-  // db: {
-  //   databaseUrl: process.env.DATABASE_URL,
-  //   ssl: true,
-  //   dialectOptions: {
-  //     ssl: {
-  //       require: true,
-  //       rejectUnauthorized: false,
-  //     },
-  //   },
-  // },
+  db: {
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
