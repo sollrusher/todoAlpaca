bf129009de5c00a6cee380503ed9b2a67aab2179
Danila Koltcov <uolcovd@gmail.com>
2021-02-25 11:53:22 +0300

    A desperate attempt №2

--- api/models/index.js
+++ api/models/index.js
@@ -16,7 +16,7 @@ console.log('sadasdassds', config)
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(config.use_env_variable, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
