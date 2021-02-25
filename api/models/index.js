a47d86088c3e7a59f63b14e9857f0c168d82b0ee
Danila Koltcov <uolcovd@gmail.com>
2021-02-25 11:49:14 +0300

    A desperate attempt

--- api/models/index.js
+++ api/models/index.js
@@ -14,7 +14,12 @@ console.log('sadasdassds', config)
 
 // console.log('ENVENVENV', env)
 
-  const sequelize = new Sequelize(config);
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
   async function gaga() {
     try {
