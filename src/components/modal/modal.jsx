4e374fe947ce9e8168918f1a8153787833e3e77d
sollrusher <sollrusher@gmail.com>
2021-03-04 15:48:47 +0300

    Fix stlyes in modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -4,22 +4,25 @@ import './modal.css';
 
 const Modal = props => {
 
-
+    let dateOfCreate = props.card.createdAt || '';
+    const regex = /[TZ]/gm;
+    dateOfCreate = dateOfCreate.replace(regex, ' ').slice(0,-5);
     return(
         <div className={`modal__wrapper ${props.isOpened ? 'open' : 'close'}`}>
             <div className='modal__body'>
                 <div className='modal__close' onClick={props.onModalClose}>X</div>
-                <h2>{props.card.title}</h2>
+                <h2 className='modal__title'>{props.card.title}</h2>
                 <hr/>
                 <input
                     name="editText"
                     type="text"
+                    className='modal__text'
                     placeholder= 'Text must be here...'
                     value={props.editText || ''}
                     onChange={props.handleChange}
                     onKeyPress={props.toggleEditText}
                 />
-                {props.card.createdAt}
+                <p className='modal__dateOfCreate'>{dateOfCreate}</p>
             </div>
         </div>
     )
