ee071f4ed69688b5b77048289bcba5fa86ca1aa5
sollrusher <sollrusher@gmail.com>
2021-03-10 01:19:04 +0300

    Rework modal to material-ui

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,31 +1,99 @@
 import './modal.css';
+import React from 'react';
+import {
+  Button,
+  Typography,
+  Dialog,
+  IconButton,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import CloseIcon from '@material-ui/icons/Close';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
 
+const styles = (theme) => ({
+  root: {
+    minWidth: 300,
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
 
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant='h6'>{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label='close'
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
 
-const Modal = props => {
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
 
-    let dateOfCreate = props.card.createdAt || '';
-    const regex = /[TZ]/gm;
-    dateOfCreate = dateOfCreate.replace(regex, ' ').slice(0,-5);
-    return(
-        <div className={`modal__wrapper ${props.isOpened ? 'open' : 'close'}`}>
-            <div className='modal__body'>
-                <div className='modal__close' onClick={props.onModalClose}>X</div>
-                <h2 className='modal__title'>{props.card.title}</h2>
-                <hr/>
-                <input
-                    name="editText"
-                    type="text"
-                    className='modal__text'
-                    placeholder= 'Text must be here...'
-                    value={props.editText || ''}
-                    onChange={props.handleChange}
-                    onKeyPress={props.toggleEditText}
-                />
-                <p className='modal__dateOfCreate'>{dateOfCreate}</p>
-            </div>
-        </div>
-    )
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+const InputLine = withStyles((theme) => ({
+    root: {
+      width: 500,
+      
+    },
+  }))(OutlinedInput);
+
+export default function CustomizedDialogs(props) {
+  return (
+    <Dialog
+      onClose={props.onModalClose}
+      aria-labelledby='customized-dialog-title'
+      open={props.isOpened}
+    >
+      <DialogTitle id='customized-dialog-title' onClose={props.onModalClose}>
+        {props.card.title}
+      </DialogTitle>
+      <DialogContent dividers>
+        <FormControl variant='outlined'>
+          <InputLabel htmlFor='component-outlined'>Your text card</InputLabel>
+          <InputLine
+            name="editText"
+            value={props.editText || ''}
+            onChange={props.handleChange}
+            label='Your text card'
+            multiline
+          />
+        </FormControl>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={props.onModalClose} color='primary'>
+          Save changes
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
-
-export default Modal
